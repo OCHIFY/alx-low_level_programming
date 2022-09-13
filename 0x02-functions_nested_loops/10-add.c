c71c46bf67b869c3c2156c0d60c180e1de86c0b5
OCHIFY <orahchiedoziem@gmail.comorahchiedoziem@gmail.com>
2022-09-13 10:17:27 -0700

    mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - sum of two integers
+ *
+ * @num1: first opperand
+ * @num2: second opperand
+ *
+ * Return: integer.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
